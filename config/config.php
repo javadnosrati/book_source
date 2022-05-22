6bbd99c80335d3bbacd7266e35186e5171d19d93
ashkan fekri <ashkanfekridev@gmail.com>
2022-05-22 04:50:52 +0430

    update config.php

--- config/config.php
+++ config/config.php
@@ -1,9 +1,10 @@
 <?php
-$db = mysqli_connect('localhost','root','','book_source');
 
-if(!$db) {
-    echo mysqli_connect_errno();
-
-}
-ini_set('display_errors',1);
-error_reporting(E_ALL);
+define('APP_CONFIG', [
+    'DB' => [
+        'hostname' => 'localhost',
+        'username' => 'root',
+        'password' => '',
+        'database' => 'book_source'
+    ]
+]);
